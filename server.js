4f3c9737b811bbaef8f790827faff7b822d073a9
oladapo <oladapoelijah49@gmail.com>
2025-08-29 14:51:55 +0100

    fix error

--- server.js
+++ server.js
@@ -11,9 +11,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: ['http://localhost:5173'],
+    origin: '*',
+    credentials: true,
+    optionsSuccessStatus: 200,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
   })
 );
 
